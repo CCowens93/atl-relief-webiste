71b484c7777227c7a21851eead25ee7bfbb0a9aa
Cassandra Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-04-17 12:54:36 -0400

    resource form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 import Resource from './components/resource';
+import Home from './components/home'
 
 function App() {
   return (
     <div className="App">
+
       <Router>
+        <div className="navbar">
+
+        <Link to="/" className="item">Home</Link>
+        <Link to="/resource" className="item">Resources</Link>
+
+        </div>
+
+        <div className="App">
+        <Route exact path="/" component={Home} />
         <Switch>
-          <Route exact path="/" component={Resource}/>
+          <Route path="/resource" component={Resource} />
         </Switch>
+        </div>
       </Router>
 
-      
+
     </div>
   );
 }
--- client/src/components/resource.js
+++ client/src/components/resource.js
@@ -2,35 +2,140 @@ import React, { Component } from 'react'
 
 import axios from 'axios'
 
-class Resource extends Component{
+class Resource extends Component {
 
     state = {
-        name:'',
-        location:'',
-        website:'',
-        phoneNumber:'',
-        email:'',
-        description:'',
-        resourceList:[]
+        _id:'',
+        name: '',
+        location: '',
+        website: '',
+        phoneNumber: '',
+        email: '',
+        description: '',
+        resourceList: []
     }
 
-    componentDidMount(){
+    changeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        const newState = { ...this.state }
+        newState[name] = value;
+        this.setState(newState)
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('/api/resource', this.state)
+    }
+
+    componentDidMount() {
         axios.get('api/resource')
-        .then((response => {
-            this.setState({ resourceList: response.data})
-            console.log(response.data)
-        }))
-        .catch((error) => {
-            console.log(error)
-        })
+            .then((response => {
+                this.setState({ resourceList: response.data })
+                console.log(response.data)
+            }))
+            .catch((error) => {
+                console.log(error)
+            })
     }
 
-    render(){
+    render() {
 
         console.log('From render()', this.state)
-        return(
+
+        const ResourceTable = this.state.resourceList.map((resource, i) => (
+            <tr key={i}>
+                <td width={300} height={50}>{resource.name}</td>
+                <td width={300} height={50}>{resource.location}</td>
+                <td width={300} height={50}>{resource.website}</td>
+                <td width={300} height={50}>{resource.phoneNumber}</td>
+                <td width={300} height={50}>{resource.email}</td>
+                <td width={300} height={50}>{resource.description}</td>
+            </tr>
+        ))
+
+        return (
+
             <div>
-                    <p>Hello, world</p>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th width={300} height={50}>Resource</th>
+                                <th width={300} height={50}>Location</th>
+                                <th width={300} height={50}>Website</th>
+                                <th width={300} height={50}>Phone Number</th>
+                                <th width={300} height={50}>E-mail</th>
+                                <th width={300} height={50}>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ResourceTable}
+                        </tbody>
+                    </table>
+
+
+                </div>
+                <div className="ResourceForm">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            name="name"
+                            type="text"
+                            placeholder="Resource"
+                            value={this.state.resourceList.name}
+                            onChange={this.changeHandler}
+                        />
+                        <br></br>
+                        <input
+                            name="location"
+                            type="text"
+                            placeholder="Location"
+                            value={this.state.resourceList.location}
+                            onChange={this.changeHandler}
+                        />
+                        <br></br>
+                        <input
+                            name="website"
+                            type="text"
+                            placeholder="Website"
+                            value={this.state.resourceList.website}
+                            onChange={this.changeHandler}
+                        />
+                        <br></br>
+                        <input
+                            name="phoneNumber"
+                            type="number"
+                            placeholder="Phone Number"
+                            value={this.state.resourceList.phoneNumber}
+                            onChange={this.changeHandler}
+                        />
+                        <br></br>
+                        <input
+                            name="email"
+                            type="text"
+                            placeholder="Email"
+                            value={this.state.resourceList.email}
+                            onChange={this.changeHandler}
+                        />
+                        <br></br>
+                        <input
+                            name="description"
+                            type="text"
+                            placeholder="Description"
+                            value={this.state.resourceList.description}
+                            onChange={this.changeHandler}
+                        />
+                        <br></br>
+                        <input
+                            type="submit"
+                            value="Submit"
+                        />
+
+                    </form>
+
+                </div>
             </div>
         )
     }
